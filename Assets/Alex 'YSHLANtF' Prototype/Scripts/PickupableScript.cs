3e60cbeb1de53ed3b180043bb86dcabb3cbb29ff
Cori Rain <winterca@icloud.com>
2018-06-22 13:07:28 -0500

    Full upload Complete
    Fully up to date from 6/20 see discord patch notes

---
+++ Assets/Alex 'YSHLANtF' Prototype/Scripts/PickupableScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupableScript : MonoBehaviour {
+
+	//Calls its own rigidbody
+	Rigidbody ownRB;
+
+	//Sets up a Original Position and an original rotation
+	Vector3 oriPos;
+	Quaternion oriRot;
+
+
+	// Use this for initialization
+	void Start () {
+
+		oriPos = transform.position;
+		oriRot = transform.rotation;
+		ownRB = this.gameObject.GetComponent<Rigidbody> ();
+		ownRB.isKinematic = true;
+		
+	}
+	
+	// FixedUpdate
+	void FixedUpdate () {
+
+
+		
+	}
+
+	public void SetVelocity_PutDown (Vector3 newVelocity)
+	{
+		transform.position = oriPos;
+		transform.rotation = oriRot;
+		ownRB.velocity = newVelocity;
+
+	}
+		
+}
